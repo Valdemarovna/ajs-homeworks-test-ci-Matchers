9ab74a804c4f221b17aa41f68a4723dac6922be1
Valdemarovna <147051721+Valdemarovna@users.noreply.github.com>
2025-06-19 21:30:50 +0300

    Update lifeline.test.js

--- src/js/__tests__/lifeline.test.js
+++ src/js/__tests__/lifeline.test.js
@@ -1,22 +1,22 @@
-import lifeLine from '../lifeline';
+import healthColor from '../lifeline';
 
 test('should be result', () => {
   const player = { name: 'Маг', health: 100 };
-  const result = lifeLine(player);
+  const result = healthColor(player);
 
   expect(result).toBe('healthy');
 });
 
 test('should be result', () => {
   const player = { name: 'Маг', health: 50 };
-  const result = lifeLine(player);
+  const result = healthColor(player);
 
   expect(result).toBe('wounded');
 });
 
 test('should be result', () => {
   const player = { name: 'Маг', health: 5 };
-  const result = lifeLine(player);
+  const result = healthColor(player);
 
   expect(result).toBe('critical');
 });
